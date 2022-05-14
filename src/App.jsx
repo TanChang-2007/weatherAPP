ae37a9593a73159c7dd54fd1de458db19d9391a0
TanChang-2007 <tanaikchang@gmail.com>
2022-05-14 21:06:09 +0800

    another one

--- src/App.jsx
+++ src/App.jsx
@@ -16,35 +16,75 @@ function App() {
         <img className='w-20 h-20 mt-5' src="//cdn.weatherapi.com/weather/64x64/night/116.png" />
         <div className=' flex items-center justify-between'>
           <div>
-        <p className='flex items-center gap-2 text-2xl ml-1 mt-4'>
-          <Icon icon="fa6-solid:location-dot" width="25" height="25" />
-          Johor Bahru
-        </p>
-        <p className='ml-1 text-xl mt-5 font-bold tracking-wider'>Partly cloudy</p>
-        <p className='text-lg mt-2 ml-1'>Feels like 30.1°C</p>
+            <p className='flex items-center gap-2 text-2xl ml-1 mt-4'>
+              <Icon icon="fa6-solid:location-dot" width="25" height="25" />
+              Johor Bahru
+            </p>
+            <p className='ml-1 text-xl mt-5 font-bold tracking-wider'>Partly cloudy</p>
+            <p className='text-lg mt-2 ml-1'>Feels like 30.1°C</p>
           </div>
           <p className=' text-7xl'>27°C</p>
-        </div>     
+        </div>
         <img src={sun} className=' absolute w-52 h-52 top-4 right-20 ' />
         <img src={clouds} className=' absolute w-96 h-64 opacity-30 top-8 left-10' />
         <img src={clouds1} className=' absolute w-96 h-64 opacity-30 top-4 right-1 ' />
         <img src={clouds2} className=' absolute w-96 h- opacity-30 top-2 left-1 ' />
       </div>
-      <div className='w-1/2 h-full text-zinc-700'>
-        <div className='flex ml-24 items-end'>
-          <input type='text' placeholder ='Location' className="flex-1 border-b-2 border-stone-200 placeholder-zinc-300 pb-2 text-xl pt-2 focus:outline-none" />
-          <div className='w-24 h-24 bg-blue-500 flex items-center justify-center rounded-xl'>
+      <div className='w-1/2 h-full text-zinc-700 overflow-auto'>
+        <div className='flex ml-20 items-end'>
+          <input type='text' placeholder='Location' className="flex-1 border-b-2 border-stone-200 placeholder-zinc-300 pb-2 text-xl pt-2 focus:outline-none" />
+          <div className=' w-20 h-20 bg-blue-500 flex items-center justify-center rounded-xl'>
             <Icon icon="ic:baseline-search" className='w-8 h-8 text-white font-extraligth' />
-          </div>      
+          </div>
         </div>
-        <div className='m-24 mt-12'>
+        <div className='m-20 mt-12'>
           <h2 className='font-bold text-2xl'>Weather Details</h2>
-          <div className='flex items-center justify-between text-xl'/>
-          <span className='font-extralight text-zinc-500'></span>
-          <span></span>
+          <div className='flex flex-col gap-5 mt-6'>
+            <div className='flex items-center justify-between text-xl'>
+              <span className='font-extralight text-zinc-500'>Cloudy</span>
+              <span className='font-bold'>75%</span>
+            </div>
+            <div className='flex items-center justify-between text-xl'>
+              <span className='font-extralight text-zinc-500'>Humidity</span>
+              <span className='font-bold'>60</span>
+            </div>
+            <div className='flex items-center justify-between text-xl'>
+              <span className='font-extralight text-zinc-500'>Wind</span>
+              <span className='font-bold'>ESE @ 2.2km/h</span>
+            </div>
+            <div className='flex items-center justify-between text-xl'>
+              <span className='font-extralight text-zinc-500'>Presure</span>
+              <span className='font-bold'>29.8</span>
+            </div>
+            <div className='flex items-center justify-between text-xl'>
+              <span className='font-extralight text-zinc-500'>Visibility</span>
+              <span className='font-bold'>10KM</span>
+            </div>
+            <div className='flex items-center justify-between text-xl'>
+              <span className='font-extralight text-zinc-500'>Wind Gust</span>
+              <span className='font-bold'>9.4KM/h</span>
+            </div>
+          </div>
+        </div>
+        <div className='ml-20 mr-20 pb-10'>
+          <h2 className='font-bold text-2xl'>Next Day</h2>
+          <div className=' flex flex-col gap-5 mt-6'>
+            <div className='flex items-center justify-between text-xl'>
+              <span className='font-extralight text-zinc-500'>Monday</span>
+              <span className='font-bold'>May 9</span>
+            </div>
+            <div className='flex items-center justify-between text-xl'>
+              <span className='font-extralight text-zinc-500'>Tuesday</span>
+              <span className='font-bold'>May 10</span>
+            </div>
+            <div className='flex items-center justify-between text-xl'>
+              <span className='font-extralight text-zinc-500'>Wednesday</span>
+              <span className='font-bold'>May 11</span>
+            </div>
+          </div>
         </div>
       </div>
-   </div>
+    </div>
   )
 }
 
